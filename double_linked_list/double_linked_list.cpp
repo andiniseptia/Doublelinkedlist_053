0911732c252d11b1474b2622eea56662c5a58da6
andiniseptia <andiniseptiandri124@gmail.com>
2023-05-26 16:16:38 +0700

    Menambahkan search pada class DoubleLinkedList

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -72,3 +72,13 @@ void DoubleLinkedList::addNode() {
 	current->next = newNode;  //step 7
 }
 
+bool DoubleLinkedList::search(int rollNo, Node** previous, Node** current) {
+	*previous = NULL;  //step 1.a
+	*current = START;  //1.b
+	while (*current != NULL && (*current)->noMhs != rollNo) {  //step 1.c
+		*previous = *current;  //step 1.d
+		*current = (*current)->next;  //step 1.e
+	}
+	return (*current != NULL);
+}
+
