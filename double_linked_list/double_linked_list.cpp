67c9c12bf949896a95c0d950a60a95e5555dcc0f
andiniseptia <andiniseptiandri124@gmail.com>
2023-05-26 15:59:02 +0700

    Menambahkan addNode pada class DoubleLinkedList

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -24,3 +24,18 @@ public:
 	void hapus();
 	void searchData();
 };
+
+void DoubleLinkedList::addNode() {
+	int nim;
+	string nm;
+	cout << "\nEnter the roll number of student : ";
+	cin >> nim;
+	cout << "\nEnter the name of student : ";
+	cin >> nm;
+
+	Node* newNode = new Node(); //step1
+	newNode->noMhs = nim;      //step 2
+	newNode->name = nm;       //step 2
+
+
+}
